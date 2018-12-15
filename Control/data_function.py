cd4e3651b04b6b5190278eee02aed3273b66eb40
Phantom_zy <Uranus.zy@gmail.com>
2018-12-15 14:01:44 +0800

    Add read data function

--- Control/data_function.py
+++ Control/data_function.py
@@ -2,14 +2,37 @@ from os import path
 
 FILENAME = 'account.txt'
 
-def get_path():
-    current_path = path.dirname('.')
+
+def get_list():
+    data_list = []
+    file = open(FILENAME, 'r')
+    for line in file:
+        data = line.split()
+        data_list.append(data)
+    
+    file.close()
+    return data_list
+
+
+def write_list(data_list):
+    file = open(FILENAME, 'w')
+    for data in data_list:
+        string = ' '.join(data)
+        file.writelines(string)
+    
+    file.close()
+    return None
 
 
 def test():
     file = open(FILENAME, 'r')
-    print(file)
+    print(file.read())
 
 
 if __name__ == '__main__':
-    test()
+    list = get_list()
+    # for data in list:
+    #     string = ' '.join(data)
+    #     print(string)
+    write_list(list)
+    print(get_list())
