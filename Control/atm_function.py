b19a396c7eabf88ac029e50288cc53b185c51c71
Henry Liang <Uranus.zy@gmail.com>
2018-12-19 17:13:37 +0800

    Add withdraw deposit transfer function

--- Control/atm_function.py
+++ Control/atm_function.py
@@ -1,4 +1,4 @@
-import data_function
+from Control import data_function
 import Model.account
 
 def login(customer_list, input_name, input_passswd):
@@ -12,6 +12,44 @@ def login(customer_list, input_name, input_passswd):
 
 def inquiry(customer):
     return customer.get_balance
+
+
+def deposit(customer, deposit_num):
+    current_balance = float(customer.get_balance())
+    customer.set_balance(str(current_balance + float(deposit_num)))
+
+
+def withdraw(customer, withdraw_num):
+    balance = float(customer.get_balance())
+    if customer.get_vip == 'V' and withdraw_num > 3000:
+        print("WARNING: Out of the withdraw limit")
+        return False
+    elif customer.get_vip == 'N' and withdraw_num > 2000:
+        print("WARNING: Out of the withdraw limit")
+        return False
+    elif withdraw_num > balance:
+        print("ERROR: Out of the account balance")
+        return False
+    else:
+        customer.set_balance(str(balance - withdraw_num))
+        return True
+
+
+def transfer(customer, target_customer, transfer_num):
+    balance = float(customer.get_balance())
+    if customer.get_vip == 'V' and transfer_num > 20000:
+        print("WARNING: Out of the transfer limit")
+        return False
+    elif customer.get_vip == 'N' and transfer_num > 10000:
+        print("WARNING: Out of the transfer limit")
+        return False
+    elif transfer_num > balance:
+        print("ERROR: Out of the account balance")
+        return False
+    else:
+        customer.set_balance(str(balance - transfer_num))
+        target_customer.set_balance(str(float(target_customer.get_balance()) + transfer_num))
+        return True
 
 
 if __name__ == '__main__':
